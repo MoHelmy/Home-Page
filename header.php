06bfa034a31827dd5487edc1044808829058dfc5
Mohamed Helmy <hemly.sfc@gmail.com>
2016-11-14 12:59:44 -0500

    First commi
    Web page as of November 14, 2016

---
+++ header.php
@@ -0,0 +1,7 @@
+        <div class="navbar navbar-default">
+	   	 <h2 style="color: #FFFFFF;">&nbsp;Mohamed <strong>HELMY, PhD</strong></h2>
+         <h5 style="color: #FFFFFF;">&nbsp;&nbsp;Postdoctoral Researcher, The Donnelly Centre, University of Toronto </h5>
+            
+            
+    	</div>
+        <h5> <a href="index.html">&nbsp;&nbsp;Bio&nbsp;</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="positions.html">Professional Experiance</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="projects.html">Research</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="afiliations.html">Affiliations</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="grants.html">Grants & Recognition</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="publications.html">Publications</a><span style="color: #18B084 ;">&nbsp;|&nbsp;</span><a href="presentations.html">Academic Presentations</a></h5>
---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="bootstrap.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="jquery.js" ></script>
+        <script src="site.js" ></script>
+        <title>
+            Mohamed Helmy, PhD
+        </title>
+    </head>
+   
+    <body>
+    <div class="container">
+    	<?php     
+include_once('header.php');
+        ?>
+    </div>
+
+        <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+         <div class="col-md-4"><br>
+          <img src="hp1.png" width="250" class="img-rounded">
+            <h3>Contact Information</h3>
+            <p>160 College Street, Room 630 <br>
+              The Donnelly Centre<br>
+              University of Toronto<br>
+              Toronto, Ontario<br>
+              Canada, M5S 3E1<br><a href="https://www.google.com/maps?f=q&hl=en&q=160+College+St,+Toronto+ON&om=1">Map</a> <br>
+              Email: mohamed.attiashahata[AT]utoronto.ca <br>Email: helmy.sfc[AT]gmail.com </p>
+            <h3>Research Expertise</h3>
+            <p class="text-left">Computational Biology/Bioinformatics, Genomics, Proteomics, Proteogenomics, Cancer Genomics, Network Biology , Systems Biology</p>
+            <h3>Publication</h3>
+            <a href="https://scholar.google.com/citations?user=Ccx_2agAAAAJ&hl=en" target="_blank">Google Scholar Profile</a>
+            
+        </div>
+        <div class="col-md-8">
+        
+        <h3 class="text-success">BIOGRAPHY</h3>
+            <p class="text-justify">I am a Postdoc researcher at Bader Lab, The Donnelly Centre of the University of Toronto. During my MSc, Ph.D. and my postdoctoral training at Kyoto University I worked in mathematical modeling of signaling pathways, Bioinformatics/Computational Biology, Proteomics, Systems Biology, Proteogenomics, Cancer Biology, Next-generation Sequence data analysis.<br>My the current focus is developing novel methods for mutations identification in cancer, studying the disease mutations impact in protein interaction networks and gene regulatory networks. I have a special interest in developing online resources for sustainable and efficient storage of biological information See my <a href="projects">projects page</a> for more about my research focus.
+
+            </p>
+        <h3 class="text-success">EDUCATION & TRANNING</h3>
+            <p>
+            <h4><strong>Ph.D. in Systems Biology -  2012</strong></h4> 		
+            <strong>Keio University, Tokyo, Japan </strong><br>
+            Advisor: Professor Masaru Tomita<br>
+            <h4><strong>M.Sc. in Systems Biology -  2009</strong></h4> 		
+            <strong>Keio University, Tokyo, Japan </strong><br>
+            Advisor: Professor Masaru Tomita<br>
+            <h4><strong>Internship in Systems Immunology -  2007(3)~2007(9)</strong></h4> 		
+            <strong>Keio University, Tsuruoka, Japan </strong><br>
+            Systems Immunology Group<br>
+            Institute for Advanced Biosciences (IAB)<br>
+            Advisor: Professor Kumar Selvarajoo<br>
+            <h4><strong>Postgraduate Diploma in Information Technology -  2002</strong></h4> 		
+            <strong>Information Technology Inistitute (ITI), Giza, Egypt</strong><br>
+            Software Skills Development Program (SSDP)<br>
+            <h4><strong>B.Sc. in Genetics -  2000</strong></h4> 		
+            <strong>Al-Azhar University, Cairo, Egypt</strong><br>
+            Grad: Very Good with honors distinction<br>
+            </p>
+          
+       </div>
+      </div>
+           
+        </div>
+        
+    </body>
+</html>
